62b31a383ec012a282e77864bd568118bf9556a7
Magesh <mageshb@live.com>
2021-12-01 11:43:46 +0530

    MultiProcessor Organizer

---
+++ src/main/MultiProcessorOrganizer.java
@@ -0,0 +1,12 @@
+package main;
+
+public class MultiProcessorOrganizer {
+    public static void main(String[] args) {
+        System.out.print("Hello");
+        int[] processorsAbility = new int[]{3, 1, 7, 2, 4};
+        int processToComplete = 15;
+        int pendingProcesses = processToComplete;
+        int seconds = 0;
+
+    }
+}
